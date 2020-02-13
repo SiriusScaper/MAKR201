5957b7fb0d0a96b951f16eba846112e9d67b206c
Ryan Uselmann <sirius@code-machine2.lan>
2020-02-13 11:19:17 -0800

    Added prompts to swift file

--- Module-3/main.swift
+++ Module-3/main.swift
@@ -6,3 +6,21 @@ Assignment 1
 */
 
 
+// Create a boolean variable to represent a light being on or off. Use an if statement to check this variable. If it is on, print “It’s bright in here!”
+
+// Use a switch statement to check if a number is positive, zero, or negative. Print “positive”, “zero”, or “negative” accordingly.
+
+// Write an if statement that checks if a given string is longer than 8 characters. If it is, print “Password length met”. If not, print “Password is not long enough”. (hint: Strings have a "count" property, just like arrays)
+
+// Write an if statement that checks to see if an array of grocery items has more than 3 elements. If it does, remove the last one. If it doesn’t, append another element on.
+
+// Write a switch statement that checks an integer from 1 to 7. Have it print the corresponding day of the week. (Ex. “1” -> “Sunday”, “2” -> “Monday”, etc.)
+
+// Check to see if an array of integers contains the number 4. If it does, set a variable “containsFour” equal to true. If it doesn’t set “containsFour” equal to false.
+
+// Create a variable “needsFuel” as a Boolean. Create another variable, “fuelLevel” to be an integer between 1 and 10. Use a ternary operator to check if fuelLevel is above 3. If it is, set needsFuel to false. If it isn’t, set needsFuel to true.
+// Create a string variable called “password” and a boolean variable called “loginPressed”. Check to see if password is equal to “abc123” and if loginPressed is equal to true. If so, then print “You are logged in”. If loginPressed is true, but the password doesn’t match, print “Incorrect password”. Otherwise, print “waiting for the user”.
+
+// Create a string variable that is only a single letter. Check to see if it is a vowel or consonant and print “This is a vowel” or “this is a consonant” accordingly. (hint: in a switch statement you can check for multiple cases by separating them with a comma)
+
+// Create two integer variables and one boolean variable called “numbersEqual”. Check if the integers are equal, and if so set numbersEqual to true. If not, set numbersEqual to false.
