d23dfdbc33e2b8ab516548dd80763054f4b764d4
Ryan Uselmann <sirius@code-machine2.lan>
2020-02-19 16:12:12 -0800

    Adding next module assignment

---
+++ Module-4/main.swift
@@ -0,0 +1,31 @@
+/* 
+Ryan Uselmann
+MAKR 201
+Module 3
+Assignment 1
+*/
+
+
+// 1. You want to make a raised garden bed, but you only have enough mulch to cover 30 square feet. You need to know the possible dimensions that you could make this garden bed so that it doesn’t have an area greater than 30 sq. feet. Assign possible side lengths to two different variables and then use an if statement to test the area of the garden bed (remember that area is just the side lengths multiplied by each other). If the area is less than or equal to 30, print “These side lengths work”. Otherwise, print “The area is too big”.
+
+
+
+// 2. Assign an integer to a variable. Write an if-then statement that tests to see if the integer is between 20 and 50. Print “Within bounds” if it is, and “Out of bounds” if it isn’t.
+
+
+
+// 3. Use a for loop to add all of the numbers between 21 and 59. Print the result.
+
+
+
+// 4. Create a for loop that loops over an array of names. If the name starts with an “a” or a “j”, add it to another array called “ajNames”. Print the ajNames array when finsished. Use the following array of names:
+
+
+
+[“alex”, “brad”, “jody”, “sara”, “juan”, “mia”, “andrey”, “michelle”, “sasha”]
+
+// 5. Use a loop to add all of the letters in this array into one string. Print the result.
+
+
+
+[“co”, “nc”, “at”, “en”, “at”, “ion”]
