e5f88e36f727223a6ef0fa4d2f3d692408856c5f
Ryan Uselmann <sirius@code-machine2.lan>
2020-02-27 11:05:10 -0800

    Working on next module

---
+++ Module-5/Module5A1.swift
@@ -0,0 +1,37 @@
+/* 
+Ryan Uselmann
+MAKR 201
+Module 4
+Assignment 
+*/
+
+
+// For all functions, include a function call. If the function returns a value, assign it to a variable.
+
+// 1. Write a function that prints out your name.
+
+func myName (name: String) {
+  print("My name is \(name)")
+}
+
+myName(name: "Ryan")
+
+// 2. Write a function that takes two strings as parameters and prints them out together.
+
+func twoParams ()
+
+// 3. Write a function that takes an integer and returns the squared value.
+
+// 4. Write a function that takes an integer as an input and prints all of the even numbers up till the given input.
+
+// 5. Write a function that takes two integers and returns true if the first integer is greater, false if the second integer is greater.
+
+// 6. Create an array of a few items that would go on a grocery list. Use a for loop and a function to loop over the array items and print them out if they start with the letter “b”.
+
+// 7. Write a function that takes an integer value and returns the sum of all number up to the input.
+
+// 8. Write a function that takes an integer and returns that Fibonacci number. Look up Fibonacci numbers if you aren’t sure what they are.
+
+// 9. Write a function that takes three integers and prints the largest one.
+
+// 10. Write a function that takes a string and prints out “What does (word) mean?”
